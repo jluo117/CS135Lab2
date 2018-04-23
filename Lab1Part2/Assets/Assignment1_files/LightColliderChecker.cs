c1f0c3d3fdfd2ef772c2842ea3c1f0516eda840e
James Luo <luojames52@gmail.com>
2018-04-23 13:09:04 -0700

    file upload

---
+++ Lab1Part2/Assets/Assignment1_files/LightColliderChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightColliderChecker : MonoBehaviour {
+    private bool isColliding = false;
+    public bool getIsColliding()
+    {
+        return isColliding;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        isColliding = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isColliding = false;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Lab1Part2/Assets/Assignment1_files/LightGameScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightGameScript : MonoBehaviour {
+    public List<GameObject> boxColliders;
+    public List<GameObject> lights;
+    public GameObject textScore;
+    public static float DEFAULT_COUNTDOWN = 5.0f;
+    public float countdown = DEFAULT_COUNTDOWN;
+    public int curLight = 0;
+    public int score = 0;
+    public System.Random rnd = new System.Random();
+
+    public void setUpNextLight()
+    {
+        lights[curLight].GetComponent<Light>().enabled = false;
+        incrementCurLight();
+        lights[curLight].GetComponent<Light>().enabled = true;
+        countdown = DEFAULT_COUNTDOWN;
+    }
+
+    public void incrementCurLight()
+    {
+        int newLight = rnd.Next(0, lights.Count - 1);
+        while(newLight == curLight)
+        {
+            newLight = rnd.Next(0, lights.Count - 1);
+        }
+        curLight = newLight;
+    }
+
+    // Use this for initialization
+    void Start () {
+		for(int i = 0; i < lights.Count; i++)
+        {
+            lights[i].GetComponent<Light>().enabled = false; 
+        }
+	}
+
+    void lightLogic()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+        {
+            setUpNextLight();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        OVRInput.FixedUpdate();
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            if (!Application.isEditor)
+            {
+                Application.Quit();
+            }
+        }
+        if (OVRInput.GetDown(OVRInput.Touch.One)){
+            if (boxColliders[curLight].GetComponent<LightColliderChecker>().getIsColliding())
+            {
+                setUpNextLight();
+                score++;
+                textScore.GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+            }
+        }
+        
+    }
+}
